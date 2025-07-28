3c05ea60ed7e5a95b143525dead447a0d37c096b
regmiganesh <regmiganesh51@gmail.com>
2025-07-28 08:57:56 +0545

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Myclass
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+    public static void Main()
+    {
+        Myclass c1 = new Myclass();
+        int result = c1.Add(10, 20);
+        Console.WriteLine(result);
+    }
+}
