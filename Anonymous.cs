b154b83e5ce2ba6937b61070f0c9ae9162514846
regmiganesh <regmiganesh51@gmail.com>
2025-08-06 09:59:05 +0545

    identifiers

--- Anonymous.cs
+++ Anonymous.cs
@@ -1,16 +1,24 @@
-using System;
+// using System;
 
-class Program
-{
-    public delegate void ShowMethod(string message);
+// class Program
+// {
+//     public delegate void ShowMessage(string name);
+//     public delegate void Message();
 
-    public static void Main()
-    {
-        ShowMethod show = delegate (string msg)
-        {
-            Console.WriteLine("Hello" + msg);
-        };
-        show("This is anonymous method");
-    }
+//     public static void Main()
 
-}
+
+//     {
+//         Message gan = delegate
+//         {
+//             Console.WriteLine("this is anonymous without parameters");
+//         };
+//         gan();
+
+//         ShowMessage show = delegate (string name)
+//         {
+//             Console.WriteLine("Hello" + name);
+//         };
+//         show("this is anonymous method");
+//     }
+// }
--- obj/Debug/net9.0/first.AssemblyInfo.cs
+++ obj/Debug/net9.0/first.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("first")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0f0ef2a684bdd9583f2e8335953c0fbb69e1da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f9f97b73095d62e0c6280ac4dd388faa0caa16a")]
 [assembly: System.Reflection.AssemblyProductAttribute("first")]
 [assembly: System.Reflection.AssemblyTitleAttribute("first")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
